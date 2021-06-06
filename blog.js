47b201afcd1669336ca4722365c41a24c4e81e14
Quentin Nicoud <quentin.nicoud@gmail.com>
2021-06-06 19:47:56 +0200

    Add minimal content in about and contac page and update home page layout

--- blog.js
+++ blog.js
@@ -1,22 +1,36 @@
+// import required packages.
 const express = require("express");
 const ejs = require("ejs");
 
 
+// Generate the app object.
 const app = express();
 
+// Allow ejs to be used by the app.
 app.set("view engine", "ejs");
 
+/* Allows to parse input from client (next line) &
+ define a directory to store the static content 
+ that will be send at each request (2nd next line). */
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// When root is requested, send the home page of the blog.
 app.get('/', (req, res) => {
     res.render("home");
 });
 
+// When /contact is requested, send the 'about me' page.
+app.get('/about', (req, res) => {
+    res.render("about");
+});
 
+// When root is requested, send the contact page.
+app.get('/contact', (req, res) => {
+    res.render("contact");
+});
 
-
-
+// Nodejs server set to listen on port 3000
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
